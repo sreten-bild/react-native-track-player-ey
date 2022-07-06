8436156f17a88e158e73ad8fc6e5cb101e133ee3
Sreten Madzgalj <sreten.madzgalj@bild-studio.net>
2022-07-06 11:32:33 +0200

    Fix setAttributes

--- android/src/main/java/com/guichaguri/trackplayer/service/MusicManager.java
+++ android/src/main/java/com/guichaguri/trackplayer/service/MusicManager.java
@@ -136,7 +136,7 @@ public class MusicManager implements OnAudioFocusChangeListener {
                 .build();
 
         player.setAudioAttributes(new com.google.android.exoplayer2.audio.AudioAttributes.Builder()
-                .setContentType(C.CONTENT_TYPE_MUSIC).setUsage(C.USAGE_MEDIA).build());
+                .setContentType(C.CONTENT_TYPE_MUSIC).setUsage(C.USAGE_MEDIA).build(), true);
 
         return new LocalPlayback(service, this, player, cacheMaxSize, autoUpdateMetadata);
     }
